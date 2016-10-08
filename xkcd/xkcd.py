9346c9fa291649a58810304d147cb76af3c19bc1
Rounak Banik <rounakbanik@gmail.com>
2016-10-09 00:24:18 +0530

    Add xkcd Comic Downloader.

---
+++ xkcd/xkcd.py
@@ -0,0 +1,32 @@
+#Use Python 3 to run script
+import bs4, requests, os, sys
+
+url = 'http://xkcd.com/1029'
+home = 'http:'
+os.makedirs('xkcd', exist_ok=True)
+
+while not url.endswith('#'):
+	print('Downloading page %s...' % url)
+	res = requests.get(url)
+	res.raise_for_status()
+	soup = bs4.BeautifulSoup(res.text)
+
+	comicElem = soup.select('#comic img')
+	if comicElem == []:
+		print('Could not find comic image.')
+	else:
+		comicUrl = home + comicElem[0].get('src')
+
+		print('Downloading image %s...' % (comicUrl))
+		res = requests.get(comicUrl)
+		res.raise_for_status()
+
+		imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
+		for chunk in res.iter_content(100000):
+			imageFile.write(chunk)
+		imageFile.close()
+	
+	prevLink = soup.select('a[rel="prev"]')[0]
+	url = 'http://xkcd.com' + prevLink.get('href')
+
+print('Done.')
---
+++ xkcd/xkcd_sort.py
@@ -0,0 +1,23 @@
+import os, sys, shutil
+
+counter = 0
+volume = 0
+for filename in os.listdir("xkcd"):
+	if counter%200 == 0:
+		volume = volume + 1
+		os.makedirs("xkcd_" + str(volume), exist_ok=True)
+
+	shutil.move("xkcd/" + str(filename), "xkcd_" + str(volume) + "/" + str(filename))
+	counter = counter + 1
+
+print("Done.")
+print(volume)
+
+for i in range(1, volume+1):
+	os.chdir("xkcd_" + str(i))
+	pdf_name = "xkcd_" + str(i) + ".pdf"
+	os.system("convert *.jpg *.gif *.png " + str(pdf_name))
+	os.chdir("..")
+	print(pdf_name + " is complete.")
+
+
